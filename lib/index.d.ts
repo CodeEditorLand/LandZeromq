03ce67d1a65116445f6bf10a29f7dd131dbce63d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-12 02:06:37 +0300

    squash!

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-/**
- * Downloads the ZMQ binaries.
- *
- * @return {*}  {Promise<void>}
- */
-export declare function downloadZMQ(): Promise<void>;
